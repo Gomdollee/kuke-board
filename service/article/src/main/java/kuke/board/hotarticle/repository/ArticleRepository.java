98f8cb9c74332ac4808e56a8c5c432b188c1c992
junwon_lee <wjdrbs307@naver.com>
2026-01-15 01:34:39 +0900

    게시판 Entity, Repository, Service 구성 및 Request, Response 작성
    + 테스트 케이스 작성 및 docker 오류 해결

--- service/article/src/main/java/kuke/board/hotarticle/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package kuke.board.hotarticle.repository;
-
-import kuke.board.hotarticle.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
